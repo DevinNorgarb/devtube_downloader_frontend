0ac5bf31cbb5bff8179cd0a73057b025285875c7
Devin <dnorgarb@gmail.com>
2020-07-26 08:38:39 +0200

    wip

---
+++ src/components/ClassComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <download/>
+  </div>
+</template>
+
+<script lang="vue">
+import Download from './Download.vue'
+export default {
+ components:  {
+   'download':Download
+ }
+}
+
+</script>
---
+++ src/components/Download.vue
@@ -0,0 +1,30 @@
+<template>
+
+<div>
+    <input v-model="youtubeURL" type="text">
+</div>
+
+</template>
+
+<script lang="vue">
+export default {
+name: 'download',
+mounted() {
+    console.log('mounted');
+
+},
+
+data() {
+    return {
+        youtubeURL: ''
+    }
+}
+
+
+
+}
+</script>
+
+<style>
+
+</style>
